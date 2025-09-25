fb1fe9690db1c8deb9ff38ddd9613327abbbceef
Lacilo <szaszfail@kkszki.hu>
2025-09-25 08:35:14 +0200

    feladat készen

--- Szovegek/Program.cs
+++ Szovegek/Program.cs
@@ -10,7 +10,38 @@ namespace Szovegek
     {
         static void Main(string[] args)
         {
+            // SZAVAKAT TARTALMAZÓ LISTA INICIALIZÁLÁSA
+            string[] words = {"uborka", "alma", "ananász", "medve", "autó", "szennyvízcsatorna", "országút" };
+            int uWordsN = 0;
 
+            foreach (string word in words)
+            {
+                if (word.Contains("u"))
+                {
+                    uWordsN++;
+                }
+            }
+
+            Console.WriteLine($"A 7 szóból {uWordsN} szóban található u betű");
         }
+
+
+        // EZ A FÜGGVÉNY FELESLEGESSÉ VÁLT
+        /*
+        static bool ContainsU(string word)
+        {
+            bool contains = false;
+
+            foreach (char c in word)
+            {
+                if (c == char.Parse("u"))
+                {
+                    contains = true;
+                }
+            }
+
+            return contains;
+        }
+        */
     }
 }
